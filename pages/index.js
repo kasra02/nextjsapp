2f70d04d221e10379d6853b4d0c714e1edbae422
kasra <kasra.niroumand@gmail.com>
2020-12-22 00:54:42 +0330

    welcome to index

--- pages/index.js
+++ pages/index.js
@@ -11,7 +11,7 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcddddddome to <a href="https://nextjs.org">Next.js!</a>
+          index to <a href="https://nextjs.org">Next.js!</a>
         </h1>
 
         <p className={styles.description}>
